619cc0b6165bbdf6bccbfebda60416639d808f13
richardatl5 <69731327+richardatl5@users.noreply.github.com>
2020-09-29 13:18:38 -0400

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,29 +21,29 @@ with open(budget_data, newline='') as csvfile:
 
         TotalMonths = TotalMonths + 1 # counts all the rows, since each row is limited to one month
 
-        TotalRevenue = TotalRevenue + int(row[1])
+        TotalRevenue = TotalRevenue + int(row[1])  # adds up the column total of profits/losses
 
-        revIncrease = int(row[1]) - PreviousRev
-        TotalRevChange = TotalRevChange + revIncrease
+        revIncrease = int(row[1]) - PreviousRev  # subtracts current row from the previous row and stores value in variable
+        TotalRevChange = TotalRevChange + revIncrease # adds up all the revenue change 
         PreviousRev =int(row[1])
-        AvgChange = round(TotalRevChange/TotalMonths,2)
+        AvgChange = round(TotalRevChange/TotalMonths,2) # not sure how to accurately discard the first row on a loop
 
-        if(revIncrease > GreatestIncr):
+        if(revIncrease > GreatestIncr):   # as it loops it stores the greatest rev compared to the previous rev
             GreatestIncr = revIncrease 
             GreatestDate = row[0]
-        if(revIncrease < GreatestDec):
+        if(revIncrease < GreatestDec): # as it loops it stores lowest decrease compared to the previous
             GreatestDec = revIncrease
             GreatestDecDate = row[0]
 
-print("Financial Analysis")
+print("Financial Analysis")   # prints all the variables, concatenated with string
 
 print("_________________________________")
 
 print("Total Months: " + str(TotalMonths))
 print("Total Revenue: $" + str(TotalRevenue))
 print("Average Change: $" + str(AvgChange))
-print(f" Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})")
-print(f" Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})")
+print(f"Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})")  # use of f-string for simplicity and speed
+print(f"Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})")
 
 with open('financial-analysis.txt', 'w') as text:
     text.write("Financial Analysis" + "\n")
@@ -51,8 +51,8 @@ with open('financial-analysis.txt', 'w') as text:
     text.write("Total Months: " + str(TotalMonths) + "\n")
     text.write("Total Revenue: $" + str(TotalRevenue) + "\n")
     text.write("Average Change: $" + str(AvgChange) + "\n")
-    text.write(f" Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})\n")
-    text.write(f" Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})\n")
+    text.write(f"Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})\n")
+    text.write(f"Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})\n")
 
 
 
