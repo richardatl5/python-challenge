9472cb35bc5561f9eec01cfdab5a3031760c0a90
richardatl5 <69731327+richardatl5@users.noreply.github.com>
2020-09-28 22:23:51 -0400

    pybank almost finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,9 +10,8 @@ PreviousRev = 0
 GreatestIncr = 0
 GreatestDec = 0
 AvgChange = 0
-
-
-profit = []
+GreatestDate = 0
+GreatestDecDate = 0
 
 with open(budget_data, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')  # using reader and not writer becasue we are not making changes to the data
@@ -27,14 +26,14 @@ with open(budget_data, newline='') as csvfile:
         revIncrease = int(row[1]) - PreviousRev
         TotalRevChange = TotalRevChange + revIncrease
         PreviousRev =int(row[1])
-
         AvgChange = round(TotalRevChange/TotalMonths,2)
 
-
-
-
-
-
+        if(revIncrease > GreatestIncr):
+            GreatestIncr = revIncrease 
+            GreatestDate = row[0]
+        if(revIncrease < GreatestDec):
+            GreatestDec = revIncrease
+            GreatestDecDate = row[0]
 
 print("Financial Analysis")
 
@@ -43,8 +42,17 @@ print("_________________________________")
 print("Total Months: " + str(TotalMonths))
 print("Total Revenue: $" + str(TotalRevenue))
 print("Average Change: $" + str(AvgChange))
+print(f" Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})")
+print(f" Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})")
 
-
+with open('financial-analysis.txt', 'w') as text:
+    text.write("Financial Analysis" + "\n")
+    text.write("_________________________\n")
+    text.write("Total Months: " + str(TotalMonths) + "\n")
+    text.write("Total Revenue: $" + str(TotalRevenue) + "\n")
+    text.write("Average Change: $" + str(AvgChange) + "\n")
+    text.write(f" Greatest Increase in Profits:   {GreatestDate} (${GreatestIncr})\n")
+    text.write(f" Greatest Decrease in Profits:   {GreatestDecDate} (${GreatestDec})\n")
 
 
 
